363a101908e59204a64bb225c5a7986c76032aee
SHY <ekdmbzzang@naver.com>
2022-09-29 23:57:50 +0900

    admin pannel custom
    - 튜플 요소가 하나면 쉼표 해줘야 튜플로 인식함

--- houses/admin.py
+++ houses/admin.py
@@ -3,4 +3,6 @@ from .models import House
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-  pass
+  list_display = ("name", "price_per_night")
+  list_filter = ("price_per_night", )
+  search_fields = ("address",)
--- houses/models.py
+++ houses/models.py
@@ -3,11 +3,12 @@ from django.db import models
 # Create your models here.
 class House(models.Model):
     
-    """ Model Definition for Houses """
+  """ Model Definition for Houses """
 
-    name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
-    description = models.TextField()
-    address = models.CharField(max_length=140)
+  name = models.CharField(max_length=140)
+  price_per_night = models.PositiveIntegerField()
+  description = models.TextField()
+  address = models.CharField(max_length=140)
 
-
+  def __str__(self):
+    return self.name
