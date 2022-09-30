7493b5c0aaa376e1232a502bcb1dadc501d7d63b
SHY <ekdmbzzang@naver.com>
2022-09-30 10:57:04 +0900

    Custom User model & admin

--- users/models.py
+++ users/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import datetime
 
 # Create your models here.
 class User(AbstractUser):
-  pass
+  first_name = models.CharField(max_length=150, editable=False)
+  last_name = models.CharField(max_length=150, editable=False)
+  name = models.CharField(max_length=150, default="")
+  is_host = models.BooleanField(default=False)
+  date_of_birth = models.DateField(default=datetime.date.today)
+
