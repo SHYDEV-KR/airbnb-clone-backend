ef5199a63fb3c91690d719db2cd1a38edd24f569
SHY <ekdmbzzang@naver.com>
2022-09-30 18:20:26 +0900

    Room model, admin
    - verbose_name_plural으로 복수형 이름 설정

--- rooms/models.py
+++ rooms/models.py
@@ -8,7 +8,10 @@ class Room(CommonModel):
     ENTIRE_PLACE = ("entire_place", "Entire Place")
     PRIVATE_ROOM = ("private_room", "Private Room")
     SHARED_ROOM = ("shared_room", "Shared Room")
-
+  name = models.CharField(
+    max_length=180,
+    default=""
+  )
   country = models.CharField(
     max_length=50,
     default="한국",
@@ -36,6 +39,9 @@ class Room(CommonModel):
     on_delete=models.CASCADE,
   )
   amenities = models.ManyToManyField("rooms.Amenity",)
+  
+  def __str__(self):
+    return self.name
 
 class Amenity(CommonModel):
 
@@ -49,3 +55,8 @@ class Amenity(CommonModel):
     null=True,
     blank=True
   )
+  def __str__(self):
+    return self.name
+    
+  class Meta:
+        verbose_name_plural = "Amenities"
