54d18a1bed4fa003613f39627ea217a95a936876
SHY <ekdmbzzang@naver.com>
2022-10-06 10:27:36 +0900

    rooms urls and views with parameters

--- rooms/urls.py
+++ rooms/urls.py
@@ -17,5 +17,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.say_hi),
+    path("", views.see_rooms),
+    path("<int:room_id>", views.see_room),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def say_hi(request):
+def see_rooms(request):
   return HttpResponse("hello")
+
+def see_room(request, room_id):
+  return HttpResponse(f"hello {room_id}")
